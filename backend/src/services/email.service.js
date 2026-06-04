e5262765deb6fe246f808af696223c85a16278d3
EseVic <esevic111@gmail.com>
2026-06-04 10:53:03 +0100

    fix: updated the email service

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -9,6 +9,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+    family: 4,
 });
 
 const FROM    = `"StockSense" <${process.env.SMTP_USER}>`;
